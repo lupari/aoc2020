7e63bad62759321641a6de643cc5936c62a27a46
Matti Lupari <matti.lupari@gmail.com>
2020-12-06 12:59:20 +0200

    Cleanup

--- src/test/scala/Test03.scala
+++ src/test/scala/Test03.scala
@@ -5,7 +5,7 @@ class Test03 extends flatspec.AnyFlatSpec with matchers.should.Matchers {
 
   it should "calculate correct result" in {
     Day03.partOne() should be(265)
-    Day03.partTwo() should be(3154761400l)
+    Day03.partTwo() should be(3154761400L)
   }
 
 }
