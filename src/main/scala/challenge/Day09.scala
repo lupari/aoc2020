3e504fd339eb0d3c7b83318a17988e3936d41ce7
Matti Lupari <matti.lupari@arcusys.fi>
2020-12-09 11:35:56 +0200

    Cleanup

--- src/main/scala/challenge/Day09.scala
+++ src/main/scala/challenge/Day09.scala
@@ -5,41 +5,31 @@ import scala.io.Source
 
 object Day09 {
 
-  def findTermSum(ns: List[Long], n: Long): Option[Long] = {
+  def findWeakness(numbers: List[Long], n: Long): Option[Long] = {
 
     @tailrec
-    def lookup(xs: List[Long], terms: List[Long]): Option[List[Long]] = xs match {
+    def _findWeakness(xs: List[Long]): Option[Long] = xs match {
       case Nil => None
       case h :: t =>
-        terms.sum + h match {
-          case x if x > n  => None
-          case x if x == n => Some(terms :+ h)
-          case _           => lookup(t, terms :+ h)
-        }
+        val sums = t.scan(h)(_ + _).takeWhile(_ <= n)
+        if (sums.last == n) {
+          val terms = (h :: t).take(sums.length)
+          Some(terms.min + terms.max)
+        } else _findWeakness(t)
     }
 
-    @tailrec
-    def _findTermSum(xs: List[Long]): Option[Long] = xs match {
-      case Nil => None
-      case h :: t =>
-        lookup(h :: t, Nil) match {
-          case None        => _findTermSum(t)
-          case Some(terms) => Some(terms.min + terms.max)
-        }
-    }
-
-    _findTermSum(ns)
+    _findWeakness(numbers)
   }
 
   val input: List[Long] = Source.fromResource("day09.txt").getLines().map(_.toLong).toList
 
   def partOne(): Long =
     input
-      .sliding(26)
-      .find(xs => !xs.dropRight(1).combinations(2).exists(c => c.sum == xs.last))
+      .sliding(25 + 1)
+      .find(xs => !xs.init.combinations(2).exists(_.sum == xs.last))
       .get
       .last
 
-  def partTwo(): Long = findTermSum(input, 138879426).get
+  def partTwo(): Long = findWeakness(input, 138879426).get
 
 }
