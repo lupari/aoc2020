e9d9ff3206be744a076faaee0eeafbb6cf29eb8f
Matti Lupari <matti.lupari@gmail.com>
2020-12-25 17:16:58 +0200

    Day24

--- src/main/scala/challenge/Day23.scala
+++ src/main/scala/challenge/Day23.scala
@@ -12,16 +12,14 @@ object Day23 {
       (next, a._2 :+ next)
     })
 
-  def linkCups(xs: List[Int]): (Int, Map[Int, Int]) = {
-    val link = xs.sliding(2).map(pair => (pair.head, pair.last)).toMap + (xs.last -> xs.head)
-    (xs.head, link)
-  }
+  def linkCups(xs: List[Int]): Map[Int, Int] =
+    xs.sliding(2).map(pair => (pair.head, pair.last)).toMap + (xs.last -> xs.head)
 
   def play(cups: List[Int], count: Int): Map[Int, Int] = {
-    val max        = cups.max
-    val (i, order) = linkCups(cups)
-    val arr        = Array.fill[Int](max + 1)(0)
-    for { kv <- order } yield arr(kv._1) = kv._2
+    val max     = cups.max
+    val linkage = linkCups(cups)
+    val arr     = Array.fill[Int](max + 1)(0)
+    for { kv <- linkage } yield arr(kv._1) = kv._2
 
     def move(current: Int): Int = {
       val pickup1 = arr(current)
@@ -32,28 +30,27 @@ object Day23 {
         .find(i => i != current && i != pickup1 && i != pickup2 && i != pickup3) // do not use a set lookup because of perf
         .get
       arr(current) = arr(pickup3)
-      val tmp = arr(destination)
+      arr(pickup3) = arr(destination)
       arr(destination) = pickup1
-      arr(pickup3) = tmp
       arr(current)
     }
 
-    val _ = Iterator.iterate(i)(move).drop(count).next()
+    Iterator.iterate(cups.head)(move).drop(count).next()
     arr.indices.map(i => (i, arr(i))).toMap
   }
 
   val cups: List[Int] = "364289715".map(_.asDigit).toList
 
   def partOne(): Int = {
-    val lastRound = play(cups, 100)
-    val cups2to9  = cupsIterator(lastRound, 1).drop(1).dropWhile(_._1 != 1).next()._2.init
+    val lastCups = play(cups, 100)
+    val cups2to9 = cupsIterator(lastCups, 1).drop(1).dropWhile(_._1 != 1).next()._2.init
     cups2to9.mkString.toInt
   }
 
   def partTwo(): Long = {
-    val cups2     = cups ++ ((cups.max + 1) to 1000000)
-    val lastRound = play(cups2, 10000000)
-    val cups2to3  = cupsIterator(lastRound, 1).drop(2).next()._2
+    val cups2    = cups ++ ((cups.max + 1) to 1000000)
+    val lastCups = play(cups2, 10000000)
+    val cups2to3 = cupsIterator(lastCups, 1).drop(2).next()._2
     cups2to3.map(_.toLong).product
   }
 
